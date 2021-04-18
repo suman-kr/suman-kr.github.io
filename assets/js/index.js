0c82922a62b8ef965daf0be12178017f1f806635
Suman Kumar <skcool.123bgp@gmail.com>
2021-04-18 18:54:25 +0530

    :art: Change theme

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,12 @@
-var count = 0;
-const switchTheme = () => {
-  const body = document.getElementsByTagName("body");
-  const button = document.getElementById("switch");
-  const dark = `rgba(0, 0, 0, 0) linear-gradient(to right, rgb(15, 12, 41), rgb(48, 43, 99), rgb(36, 36, 62)) repeat scroll 0% 0%`;
-  const light = `rgba(0, 0, 0, 0) linear-gradient(to right, rgb(53, 92, 125), rgb(108, 91, 123), rgb(192, 108, 132)) repeat scroll 0% 0%`;
-  const createOn = document.createElement("i");
-  createOn.setAttribute("class", "fas fa-toggle-on");
-  const createOff = document.createElement("i");
-  createOff.setAttribute("class", "fas fa-toggle-off");
-  if (count % 2 === 0) {
-    body[0].style.background = dark;
-    button.innerHTML = "";
-    button.appendChild(createOn);
-  } else {
-    body[0].style.background = light;
-    button.removeChild(button.childNodes[0]);
-    button.appendChild(createOff);
-  }
+const quote = new Promise((res, err) => {
+  let quote = fetch("http://py-quoters.herokuapp.com");
+  res(quote.then((e) => e.json()));
+  err("URL down");
+});
 
-  count += 1;
-};
+
+setTimeout(() => {
+  quote.then((e) => {
+    document.getElementById("quote").innerText = e.quote;
+  });
+}, 500);
